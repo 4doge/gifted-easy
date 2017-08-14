f9785f2a145dae2e8f5395fd3aad3983635f6b3a
Boksha Anton <antonboksha@gmail.com>
2017-08-14 17:19:39 +0300

    fix build docker command

--- build_files/docker.sh
+++ build_files/docker.sh
@@ -3,6 +3,6 @@
 # TODO: also move credentials to secret file on S3
 
 docker login -e $DOCKER_CLOUD_EMAIL -u $DOCKER_CLOUD_USERNAME -p $DOCKER_CLOUD_PASSWORD
-docker build -t giftedeasy/gifted
+docker build -t giftedeasy/gifted .
 docker push giftedeasy/gifted
 
