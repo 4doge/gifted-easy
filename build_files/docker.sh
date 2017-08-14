834b58da510d4abf6d789057443e7237a612fe61
Boksha Anton <antonboksha@gmail.com>
2017-08-14 17:06:01 +0300

    add email arg to docker login

--- build_files/docker.sh
+++ build_files/docker.sh
@@ -2,7 +2,7 @@
 
 # TODO: also move credentials to secret file on S3
 
-docker login -u $DOCKER_CLOUD_USERNAME -p $DOCKER_CLOUD_PASSWORD
+docker login -e $DOCKER_CLOUD_EMAIL -u $DOCKER_CLOUD_USERNAME -p $DOCKER_CLOUD_PASSWORD
 docker build -t giftedeasy/gifted
 docker push giftedeasy/gifted
 
