4e97718a13fa82affe3e680a22bce74fd06c6b70
Boksha Anton <antonboksha@gmail.com>
2017-09-01 20:38:24 +0300

    add latest tag to docker cloud repo

--- build_files/deploy.sh
+++ build_files/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker login -e $DOCKER_CLOUD_EMAIL -u $DOCKER_CLOUD_USERNAME -p $DOCKER_CLOUD_PASSWORD
-docker build -t giftedeasy/gifted .
-docker push giftedeasy/gifted
-
+docker build -t giftedeasy/gifted:latest .
+docker push giftedeasy/gifted:latest
